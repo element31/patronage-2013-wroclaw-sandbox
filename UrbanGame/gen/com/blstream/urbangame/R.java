59f210fd648ac97d1c0a01953b18e45f97540278
Lukasz Tomaszewski <element131@gmail.com>
2013-04-13 16:10:01 +0200

    Added log in view

--- UrbanGame/gen/com/blstream/urbangame/R.java
+++ UrbanGame/gen/com/blstream/urbangame/R.java
@@ -24,10 +24,14 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080003;
+        public static final int checkBoxShowPassword=0x7f080002;
+        public static final int editTextEmail=0x7f080000;
+        public static final int editTextPassword=0x7f080001;
     }
     public static final class layout {
         public static final int activity_start=0x7f030000;
+        public static final int loggin_in=0x7f030001;
     }
     public static final class menu {
         public static final int start=0x7f070000;
@@ -35,7 +39,14 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int button_log_in=0x7f050007;
+        public static final int button_register=0x7f050006;
+        public static final int checkbox_show_password=0x7f050005;
         public static final int hello_world=0x7f050002;
+        /**  Logging in 
+         */
+        public static final int hint_email=0x7f050003;
+        public static final int hint_password=0x7f050004;
     }
     public static final class style {
         /** 
